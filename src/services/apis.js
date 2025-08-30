46ff379c2e7863b5d521b5cc270fff5fca29618e
sachin-hy <sachin1307saini@gmail.com>
2025-08-30 20:10:53 +0530

    Updated Base Url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://elearning-platform-frontend-rho.vercel.app";
 
 export const categories = {
     CATEGORIES_API:BASE_URL+"/course/showAllCategories",
